cd094559e37d7a4b8b64c0c022c697c48c02b53d
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2018-05-02 10:48:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace moving_elephants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
